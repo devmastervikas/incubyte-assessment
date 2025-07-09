d36c1637b5075a52bff0cb026a095fef95ce97d0
Vikas Singh <devmaster.vikas@gmail.com>
2025-07-09 12:31:08 +0530

    support newline as delimiter in test file and chnaged the testcase input attribute

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -7,7 +7,7 @@
 const { add } = require("../src/calculator");
 
 describe("String Calculator", () => {
-  test("should return sum of multiple comma-separated numbers", () => {
-    expect(add("1,2,3,4")).toBe(10);
+  test("should handle newline as delimiter along with commas", () => {
+    expect(add("1\n2,3")).toBe(6);
   });
 });
