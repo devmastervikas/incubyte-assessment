7f1be4d153ba7ed3fd7f85bde1bd59cad7043ca9
Vikas Singh <devmaster.vikas@gmail.com>
2025-07-09 17:25:55 +0530

    change the test case for the support custom delimiter

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -7,7 +7,7 @@
 const { add } = require("../src/calculator");
 
 describe("String Calculator", () => {
-  test("should handle newline as delimiter along with commas", () => {
-    expect(add("1\n2,3")).toBe(6);
+  test("should support custom delimiter syntax //;\n1;2", () => {
+    expect(add("//;\n1;2")).toBe(3);
   });
 });
