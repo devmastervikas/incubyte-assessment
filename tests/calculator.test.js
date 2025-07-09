b60b62955cb26b928928fe01eb66bf1461317f63
Vikas Singh <devmaster.vikas@gmail.com>
2025-07-09 11:27:10 +0530

    test: add two comma-separated numbers

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -6,7 +6,7 @@
 const { add } = require("../src/calculator");
 
 describe("String Calculator", () => {
-  test("should return number when input has one number", () => {
-    expect(add("5")).toBe(5);
+  test("should return sum of two comma-separated numbers", () => {
+    expect(add("1,2")).toBe(3);
   });
 });
