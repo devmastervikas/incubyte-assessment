8b731ac283b60d95b2b155bdd601e9a39cbf5891
Vikas Singh <devmaster.vikas@gmail.com>
2025-07-09 17:24:09 +0530

    change the logic for the support custom single-character delimiter syntax

--- src/calculator.js
+++ src/calculator.js
@@ -8,7 +8,15 @@
  */
 function add(numbers) {
   if (numbers === "") return 0;
-  const values = numbers.split(/,|\n/).map(Number);
+  let delimiter = /,|\n/;
+
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(parts[0].slice(2));
+    numbers = parts[1];
+  }
+
+  const values = numbers.split(delimiter).map(Number);
   return values.reduce((sum, val) => sum + val, 0);
 }
 
